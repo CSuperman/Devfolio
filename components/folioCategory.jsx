77b442892be70d5cecaf16c539b3f5216151cc78
CSuperman <crlbusiness1@gmail.com>
2024-02-04 22:53:47 -0500

    Create folioCategory.jsx

---
+++ components/folioCategory.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+const hrPortfolio = {
+  name: "HR Portfolio",
+  url: "https://www.google.com",
+  items: [
+    { name: "Portfolio Item 1" },
+    { name: "Portfolio Item 2" },
+  ],
+};
+
+const nerdFolio = {
+  name: "nerdfolio",
+  url: "https://www.google.com",
+  items: [
+    { whatever: "Nerdfolio Item 1" },
+    { whatever: "Nerdfolio Item 2" },
+  ],
+};
+
+export default function MyButton() {
+  const [hoveredType, setHoveredType] = useState(null);
+
+
+  function handleHover(type) {
+    setHoveredType(type);
+  }
+
+  function handleMouseLeave() {
+    setHoveredType(null);
+  }
+
+  return (
+    <div onMouseLeave={handleMouseLeave}>
+      <button onMouseEnter={() => handleHover("HR")}>HR Portfolio</button>
+      {hoveredType === "HR" && (
+        <>
+          <a href={hrPortfolio.url} target="_blank" rel="noopener noreferrer">
+            {hrPortfolio.name}
+          </a>
+          <ul>
+            {hrPortfolio.items.map((item) => (
+              <li key={item.name}>{item.name}</li>
+            ))}
+          </ul>
+        </>
+      )}
+      <button onMouseEnter={() => handleHover("nerds")}>Nerdfolio</button>
+      {hoveredType === "nerds" && (
+        <>
+          <a href={nerdFolio.url} target="_blank" rel="noopener noreferrer">
+            {nerdFolio.name}
+          </a>
+          <ul>
+            {nerdFolio.items.map((item) => (
+              <li key={item.whatever}>{item.whatever}</li>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+}
