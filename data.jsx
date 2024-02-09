0340d38205c6075ef56982ee9289c37639abdc03
CSuperman <crlbusiness1@gmail.com>
2024-02-08 23:03:33 -0500

    Create data.jsx

---
+++ data.jsx
@@ -0,0 +1,71 @@
+// This will be the nerds section
+
+// duplicate the data structuring for HR portfolio
+
+
+
+export const navLinks = [
+  {
+    id: "foryou",
+    title: "Your Solutions",
+  },
+  {
+    id: "video",
+    title: "Video",
+  },
+  {
+    id: "gifs",
+    title: "GIFs",
+  },
+  {
+    id: "BEP",
+    title: "Backend Portfolio",
+    url: "https://www.clark.com/backend",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+
+// referenced into 1 location
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Talented if you look closely.",
+    name: "John Hemaz",
+    title: "Business Owner",
+    img: person01,
+    proof: "https://www.proof.com",
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Very hard worker that you would be lucky to have on your team.",
+    name: "Steve Martin",
+    title: "Director",
+    img: person02,
+    proof: "https://www.proof.com",
+  },
+];
+
+
+
+export const footerLinks = [
+  {
+    title: "End Links",
+    links: [
+      {
+        name: "Blog",
+        link: "https://www.clark.com/blog/",
+      },
+      {
+        name: "Talents",
+        link: "https://www.clark.com/talents/",
+      },
+    ],
+  },
+]
