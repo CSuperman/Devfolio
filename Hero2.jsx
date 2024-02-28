9c2afe310bb5b944bbcb1ea34d8d2354a56ba038
CSuperman <crlbusiness1@gmail.com>
2024-02-27 23:03:38 -0500

    Create Hero2.jsx
    2nd example hero section in development

---
+++ Hero2.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useRef } from "react";
+
+export default function Hero() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      Delay: 1333,
+      Speed: 57,
+      strings: ["Developer", "AI", "Mobile"],
+    });
+  }, []);
+
+  return (
+    <div className="hero" id="hero">
+      <div className="left">
+        <div className="img">
+          <img src="" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Heyyy There! I'm</h2>
+          <h1>Richard Lipkovitz</h1>
+          <h4>
+            Freelance <span ref={textRef}></span>
+          </h4>
+        </div>
+        <a href="#portfolio">
+          <img src="" alt="" />
+        </a>
+      </div>
+    </div>
+  );
+}
